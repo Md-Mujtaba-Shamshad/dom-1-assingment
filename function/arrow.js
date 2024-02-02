a5ec994685f1b7652db16b2971c77617297b5f18
Md Mujtaba Shamshad <bazmijn@gmail.com>
2024-02-02 14:51:43 +0530

    dom events & click

---
+++ function/arrow.js
@@ -0,0 +1,10 @@
+let game = function()
+{
+    console.log("game downloaded")
+}
+function strat(x,open)
+{
+    x()
+    console.log("please wait game is proccessing",open)
+}
+strat(game,"super mario")
